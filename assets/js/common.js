7dda7e519c3479477179235768d5c5ec0d0b0b4a
soH-0911 <wjdthgus2320@naver.com>
2024-11-13 16:46:25 +0900

    241113-수정본

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,6 +1,3 @@
-// 마우스 스크롤 애니메이션 스타트 함수
-// AOS.init();
-
 $(function(){
 /****** 헤더 ******/
     // 스크롤시 헤더 색상 fill
@@ -29,29 +26,6 @@ $(function(){
         $('.gnb .dep1-menu').removeClass('on');
     });
 
-    // $('.gnb .dep1-menu').mouseover(function(){
-    //     $('header').css({'background-color': 'white', 'border-bottom':'1px solid var(--color-gray100)'});
-    //     $('.header-wrap').css({'border-bottom':'1px solid var(--color-gray100)'});
-    //     $('.util-menu, .header-left').css({'color':'var(--color-gray800)'});
-    //     $('.header-gnb-sub').css({'display':'block'});
-
-    //     let dep1Idx = $(this).index();
-    //     // console.log($('.dep1-menu'));
-    //     $('.gnb-sub-wrap .dep2-wrap').hide()
-    //     $('.gnb-sub-wrap .dep2-wrap').eq(dep1Idx).show() // 인덱스 번호가 같은것끼리 show처리
-    //     $('.gnb .dep1-menu').removeClass('on');
-    //     $(this).addClass('on');
-
-    // });
-    // 마우스 메뉴에서 나갈때 gnb-sub 안보이게
-    // $('.header').mouseleave(function(){
-    //     $('.header-gnb-sub').css({'display':'none'});
-    //     // $('header').css({'background': 'none','border-bottom':'0'});
-    //     $('.header-wrap').css({'border':'0'});
-    //     $('.util-menu, .header-left').css({'color':'#fff'});
-    //     $('.dep1-menu').removeClass('on');
-    // });
-
     // 헤더 배너 스와이퍼
     var swiper = new Swiper(".header-swiper", {
         slidesPerView: 1,
@@ -65,9 +39,6 @@ $(function(){
           prevEl: ".header-btn-prev",
         },    
         loop: true,
-        // pagination: {
-        //     el: ".swiper-pagination",
-        // }
       });
 
     
